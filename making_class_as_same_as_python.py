412782ba17c466e9d309f0d8cda4526d75bd96ea
moksh <moksh@Mokshs-MacBook-Air.local>
2025-08-05 22:09:07 +0530

    add

---
+++ making_class_as_same_as_python.py
@@ -0,0 +1,9 @@
+# here we will make a python list using c array
+import ctypes
+
+class ctypes:
+    def __init__(self):
+        self.size = 1     # size is how many you can store
+        self.n = 0        # n = no. of element stores
+        self.A = self.__make_array(self.size)
+        
